ff6d0d247b48ac79cd34cbaa4177631942319db4
ssoboliev <ssoboliev@cybervisiontech.com>
2016-09-06 22:12:06 +0300

    Realizaiton of KFrequent with priority queue

--- src/main/java/com/sbk/leetcode/heap/TopKFrequent.java
+++ src/main/java/com/sbk/leetcode/heap/TopKFrequent.java
@@ -9,15 +9,12 @@ class TopKFrequent {
             int count = numsCount.containsKey(n) ? numsCount.get(n)+1 : 1;
             numsCount.put(n, count);
         }
-        List<Map.Entry<Integer, Integer>> sortedEntries = new ArrayList<>(numsCount.entrySet());
-
-        Collections.sort(sortedEntries,
-                (e1, e2) -> e2.getValue().compareTo(e1.getValue())
-        );
-        List<Integer> result = new ArrayList<>();
-        for(int i=0; i<k; i++) {
-            result.add(sortedEntries.get(i).getKey());
+        Queue<Map.Entry<Integer, Integer>> q = new PriorityQueue<>((o1, o2) -> o2.getValue().compareTo(o1.getValue()));
+        q.addAll(numsCount.entrySet());
+        List<Integer> res = new ArrayList<>();
+        for(int i=0;i<k;i++) {
+            res.add(q.poll().getKey());
         }
-        return result;
+        return res;
     }
 }
