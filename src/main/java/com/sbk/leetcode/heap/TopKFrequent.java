9db423cfcc330da335a6a4db223b27cff0755473
ssoboliev <ssoboliev@cybervisiontech.com>
2016-09-06 22:29:26 +0300

    Stream api decision(slow but compact)

--- src/main/java/com/sbk/leetcode/heap/TopKFrequent.java
+++ src/main/java/com/sbk/leetcode/heap/TopKFrequent.java
@@ -1,27 +1,18 @@
 package com.sbk.leetcode.heap;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 class TopKFrequent {
     List<Integer> topKFrequent(int[] nums, int k) {
-        List<Integer>[] bucket  = new List[nums.length+1];
-        Map<Integer, Integer> numsCount = new HashMap<>();
-        for(int n : nums) {
-            numsCount.put(n, numsCount.getOrDefault(n,0)+1);
-        }
-        for(int key: numsCount.keySet()) {
-            int freq = numsCount.get(key);
-            if(bucket[freq] == null) {
-                List<Integer> freqList = new ArrayList<>();
-                bucket[freq] = freqList;
-            }
-            bucket[freq].add(key);
-        }
-        List<Integer> res =  new ArrayList<>();
-        for(int i=bucket.length-1; i>=0 && res.size()<k; i--) {
-            if(bucket[i] == null) continue;
-            res.addAll(bucket[i]);
-        }
-        return res;
+        Map<Integer, Long> map = Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.groupingBy(p->p,Collectors.counting()));
+        List<Map.Entry<Integer,Long>> entryList = new ArrayList<>(map.entrySet());
+        return entryList.stream()
+                .sorted((p,q) -> (q.getValue().intValue() - p.getValue().intValue()))
+                .limit(k)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 }
