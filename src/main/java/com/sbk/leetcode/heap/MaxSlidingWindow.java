0d572ff2ee56e229c4a306160b2c917924517e49
ssoboliev <1nt3gr@L>
2016-09-09 14:33:09 +0300

    155ms MaxSlidingWindowSolution

--- src/main/java/com/sbk/leetcode/heap/MaxSlidingWindow.java
+++ src/main/java/com/sbk/leetcode/heap/MaxSlidingWindow.java
@@ -1,10 +1,24 @@
 package com.sbk.leetcode.heap;
 
+import java.util.Arrays;
+
 /**
  * Created by User on 06.09.2016.
  */
 public class MaxSlidingWindow {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        return new int[nums.length];
+        if(nums.length == 0) return nums;
+        int slidingWindowSize = nums.length - k+1;
+        int[] slidingWindow = new int[slidingWindowSize];
+        for(int i = 0; i< slidingWindowSize; i++) {
+            int[] window = findWindow(nums, i, k);
+            slidingWindow[i] = Arrays.stream(window).max().getAsInt();
+        }
+        return slidingWindow;
+    }
+    private int[] findWindow(int[] nums, int shift, int k) {
+        int[] window = new int[k];
+        System.arraycopy(nums, shift, window, 0, k);
+        return window;
     }
 }
--- src/test/java/com/sbk/leetcode/heap/MaxSlidingWindowTest.java
+++ src/test/java/com/sbk/leetcode/heap/MaxSlidingWindowTest.java
@@ -2,15 +2,18 @@ package com.sbk.leetcode.heap;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class MaxSlidingWindowTest {
     private MaxSlidingWindow m = new MaxSlidingWindow();
     @Test
     public void maxSlidingWindow() throws Exception {
-        assertEquals(new int[]{5,6}, m.maxSlidingWindow(new int[]{2,3,4,5,6},2));
-        assertEquals(new int[]{4,5,6}, m.maxSlidingWindow(new int[]{2,3,4,5,6},3));
-        assertEquals(new int[]{3,3,5,5,6,7}, m.maxSlidingWindow(new int[]{1,3,-1,-3,5,3,6,7},3));
+        assertEquals(Arrays.toString(new int[]{}), Arrays.toString(m.maxSlidingWindow(new int[]{},0)));
+        assertEquals(Arrays.toString(new int[]{3,4,5,6}), Arrays.toString(m.maxSlidingWindow(new int[]{2,3,4,5,6},2)));
+        assertEquals(Arrays.toString(new int[]{4,5,6}), Arrays.toString(m.maxSlidingWindow(new int[]{2,3,4,5,6},3)));
+        assertEquals(Arrays.toString(new int[]{3,3,5,5,6,7}), Arrays.toString(m.maxSlidingWindow(new int[]{1,3,-1,-3,5,3,6,7},3)));
     }
 
 }
