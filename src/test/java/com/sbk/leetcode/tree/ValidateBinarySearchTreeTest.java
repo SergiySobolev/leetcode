6f1578dffe1031ecf3b9c8d643740d020f5bc132
SergiySobolev <sergiy.sobolev@gmail.com>
2016-09-21 21:10:44 +0300

    ValidateBinarySearchTree valid implementation + new tests

--- src/test/java/com/sbk/leetcode/tree/ValidateBinarySearchTreeTest.java
+++ src/test/java/com/sbk/leetcode/tree/ValidateBinarySearchTreeTest.java
@@ -9,8 +9,11 @@ public class ValidateBinarySearchTreeTest {
     private ValidateBinarySearchTree vbst = new ValidateBinarySearchTree();
     @Test
     public void isValidBST() throws Exception {
+        assertFalse(vbst.isValidBST(fromArray(new Integer[]{3,1,5,0,2,4,6,null,null,null,3})));
+        assertTrue(vbst.isValidBST(fromArray(new Integer[]{-2147483648,null,2147483647})));
         assertFalse(vbst.isValidBST(fromArray(new Integer[]{10,5,15,null,null,6,20})));
         assertTrue(vbst.isValidBST(fromArray(new Integer[]{1})));
+        assertFalse(vbst.isValidBST(fromArray(new Integer[]{1,1})));
         assertFalse(vbst.isValidBST(fromArray(new Integer[]{1,2})));
         assertTrue(vbst.isValidBST(fromArray(new Integer[]{2,1})));
         assertFalse(vbst.isValidBST(fromArray(new Integer[]{1,2,3})));
@@ -19,6 +22,7 @@ public class ValidateBinarySearchTreeTest {
         assertTrue(vbst.isValidBST(fromArray(new Integer[]{2,1,3})));
         assertTrue(vbst.isValidBST(fromArray(new Integer[]{2,1,3,0})));
         assertTrue(vbst.isValidBST(fromArray(new Integer[]{5,3,7,2,4,6,8})));
+        assertFalse(vbst.isValidBST(fromArray(new Integer[]{5,3,7,2,4,4,8})));
 
     }
 
