ac3dc7dac891ab85c2156a8f293ff1e195b3baed
ssoboliev <ssoboliev@cybervisiontech.com>
2016-10-15 16:41:43 +0300

    BuildTreeFromOrderAndPostorder

--- src/main/java/com/sbk/leetcode/tree/BuildTreeFromIOAndPO.java
+++ src/main/java/com/sbk/leetcode/tree/BuildTreeFromIOAndPO.java
@@ -1,8 +1,17 @@
 package com.sbk.leetcode.tree;
 
-
 public class BuildTreeFromIOAndPO {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        return null;
+        return buildBST(postorder.length-1, 0, postorder.length-1, inorder, postorder);
+    }
+    private TreeNode buildBST(int rootPos, int inStart, int inEnd, int[] inorder, int[] postorder){
+        if(inStart > inEnd || rootPos < 0) return null;
+        int midPos=0;
+        for(int i=inStart;i<=inEnd;i++)
+            if(postorder[rootPos] == inorder[i]) midPos = i;
+        TreeNode node = new TreeNode(postorder[rootPos]);
+        node.left = buildBST(rootPos - (inEnd - midPos) - 1, inStart, midPos-1, inorder, postorder);
+        node.right = buildBST(rootPos-1, midPos+1, inEnd, inorder, postorder);
+        return node;
     }
 }
--- src/test/java/com/sbk/leetcode/tree/BuildTreeFromIOAndPOTest.java
+++ src/test/java/com/sbk/leetcode/tree/BuildTreeFromIOAndPOTest.java
@@ -10,8 +10,12 @@ public class BuildTreeFromIOAndPOTest {
     @Test
     public void buildTree_1() {
         TreeNode res = bt.buildTree(new int[]{1,2}, new int[]{2,1});
-        assertEquals(res.val, 1);
-        assertNull(res.left.val);
-        assertEquals(res.right.val, 2);
+        assertArrayEquals(TreeUtils.toArray(res), new Integer[]{1,null,2});
+    }
+
+    @Test
+    public void buildTree_2() {
+        TreeNode res = bt.buildTree(new int[]{1,2,3}, new int[]{3,2,1});
+        assertArrayEquals(TreeUtils.toArray(res), new Integer[]{1,null,2,null,null,null,3});
     }
 }
