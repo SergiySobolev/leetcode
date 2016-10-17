d8181c74cc50eee3e2d302ab2878b0ac58d098be
ssoboliev <ssoboliev@cybervisiontech.com>
2016-10-18 00:49:28 +0300

    [TREE]Root Path Sum II

--- src/main/java/com/sbk/leetcode/tree/RootToLeafPathSum.java
+++ src/main/java/com/sbk/leetcode/tree/RootToLeafPathSum.java
@@ -1,10 +1,37 @@
 package com.sbk.leetcode.tree;
 
 
-import java.util.List;
+import java.util.*;
 
 public class RootToLeafPathSum {
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
-        return null;
+        if(Objects.isNull(root)) return new ArrayList<>();
+        List<List<Integer>> allPaths = buildAllPaths(root, new ArrayList<>());
+        List<List<Integer>> res = new ArrayList<>();
+        for(List<Integer> path : allPaths){
+            int pathSum = path.stream().mapToInt(i -> i).sum();
+            if(pathSum == sum) {
+                res.add(path);
+            }
+        }
+        return res;
+    }
+
+    private List<List<Integer>> buildAllPaths(TreeNode root, List<Integer> prevPath) {
+        if(Objects.isNull(root.left) && Objects.isNull(root.right)) {
+            List<Integer> res = new ArrayList<>(prevPath);
+            res.add(root.val);
+            return Collections.singletonList(res);
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> newPrevPath = new ArrayList<>(prevPath);
+        newPrevPath.add(root.val);
+        if(!Objects.isNull(root.left)) {
+            res.addAll(buildAllPaths(root.left, newPrevPath));
+        }
+        if(!Objects.isNull(root.right)) {
+            res.addAll(buildAllPaths(root.right, newPrevPath));
+        }
+        return res;
     }
 }
--- src/test/java/com/sbk/leetcode/tree/RootToLeafPathSumTest.java
+++ src/test/java/com/sbk/leetcode/tree/RootToLeafPathSumTest.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.Assert.assertEquals;
 
 public class RootToLeafPathSumTest {
@@ -17,13 +19,13 @@ public class RootToLeafPathSumTest {
     }
     @Test
     public void sum_2() {
-        List<List<Integer>> res = rtl.pathSum(TreeUtils.fromArray(5,4,8,11,null,null,13,4,7,2,null,null,null,null,5,1),22);
+        List<List<Integer>> res = rtl.pathSum(TreeUtils.fromArray(5,4,8,11,null,13,4,7,2,null,null,null,null,5,1),22);
         assertEquals(res, asList(asList(5,4,11,2), asList(5,8,4,5)));
     }
 
     @Test
     public void sum_3() {
         List<List<Integer>> res = rtl.pathSum(TreeUtils.fromArray(5,4,6,2),11);
-        assertEquals(res, asList(asList(5,6), asList(5,4,2)));
+        assertThat(res, containsInAnyOrder(asList(5,6), asList(5,4,2)));
     }
 }
