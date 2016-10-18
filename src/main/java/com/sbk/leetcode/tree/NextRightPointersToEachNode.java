84bbaac7245b14c1b5a9c1dc7a20c5a2a710a18e
ssoboliev <ssoboliev@cybervisiontech.com>
2016-10-19 02:00:44 +0300

    [TREE] Populating Next Right Pointers in Each Node iterative implementation

--- src/main/java/com/sbk/leetcode/tree/NextRightPointersToEachNode.java
+++ src/main/java/com/sbk/leetcode/tree/NextRightPointersToEachNode.java
@@ -1,10 +1,31 @@
 package com.sbk.leetcode.tree;
 
-/**
- * Created by User on 19.10.2016.
- */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class NextRightPointersToEachNode {
     public void connect(TreeLinkNode root) {
-
+        if(Objects.isNull(root)) return;
+        List<List<TreeLinkNode>> levels = new ArrayList<>();
+        List<TreeLinkNode> curLevel = new ArrayList<>();
+        curLevel.add(root);
+        while(!curLevel.isEmpty()) {
+            List<TreeLinkNode> newLevel = new ArrayList<>();
+            for(TreeLinkNode n : curLevel) {
+                if(!Objects.isNull(n.left)) {
+                    newLevel.add(n.left);
+                    newLevel.add(n.right);
+                }
+            }
+            levels.add(curLevel);
+            curLevel = newLevel;
+        }
+        for(List<TreeLinkNode> level : levels) {
+            for(int i=0;i<level.size()-1;i++){
+                level.get(i).next = level.get(i+1);
+            }
+        }
     }
 }
